91c3b9a7eca2e3ae007dbe281abb3ab99c460a0b
Franco Reyes <francko_mik@hotmail.com>
2019-10-31 05:09:47 -0600

    Se puede borrar el lienzo

--- Dibujo.js
+++ Dibujo.js
@@ -8,70 +8,77 @@ class Dibujo{
         this.j=0;
         this.new_exp=[];
         this.op_top=0;
-        this.op_exp;
         this.front=0;
-        this.largo;
-        this.des;
         this.top=0;
         this.cola=[];
+        this.xi;
+        this.yi;
+        this.xf;
+        this.yf;
+        this.canvas = document.getElementById("lienzo");
+        this.ctx = this.canvas.getContext("2d");
+        this.valid=true;
     }
 
     //dibujo abajo
     circulo(){        
-        var canvas = document.getElementById("lienzo");
-        var ctx = canvas.getContext("2d");
-        ctx.moveTo(this.x,this.y);
-        ctx.arc(this.x-25,this.y,25,0,2*Math.PI,true);
-        ctx.stroke();
+        this.ctx.moveTo(this.x,this.y);
+        this.ctx.arc(this.x-25,this.y,25,0,2*Math.PI,true);
+        this.ctx.stroke();
         var centrox=this.x-33;
         var centroy=this.y+6;
         this.estado(centrox,centroy)
     }
     linea(){
-        var canvas = document.getElementById("lienzo");
-        var ctx = canvas.getContext("2d");
-        ctx.strokeStyle = "#ffffff";
-        ctx.fillStyle = "#ffffff";
-        ctx.moveTo(this.x,this.y);
-        ctx.lineTo(this.x+50,this.y);
+        this.ctx.strokeStyle = "#ffffff";
+        this.ctx.fillStyle = "#ffffff";
+        this.ctx.moveTo(this.x,this.y);
+        this.ctx.lineTo(this.x+50,this.y);
         this.x+=100;
     }
     flecha(simb){
-        var canvas = document.getElementById("lienzo");
-        var ctx = canvas.getContext("2d");
-        ctx.moveTo(this.x,this.y);
-        ctx.lineTo(this.x+50,this.y);
+        this.ctx.strokeStyle = "#ffffff";
+        this.ctx.fillStyle = "#ffffff";
+        this.ctx.moveTo(this.x,this.y);
+        this.ctx.lineTo(this.x+50,this.y);
         this.escribir(simb,this.x,this.y);
         this.x+=100;
-        ctx.lineTo(this.x-60,this.y-10);
-        ctx.lineTo(this.x-50,this.y);
-        ctx.lineTo(this.x-60,this.y+10);
+        this.ctx.lineTo(this.x-60,this.y-10);
+        this.ctx.lineTo(this.x-50,this.y);
+        this.ctx.lineTo(this.x-60,this.y+10);
     }
     regresar(x1,x2,y1,y2){
-        var canvas = document.getElementById("lienzo");
-        var ctx = canvas.getContext("2d");
-        ctx.moveTo(x2-25,y2-25);
-        ctx.quadraticCurveTo(((x2-50)+x1)/2,this.y-75,x1-25,y1-25);
-        ctx.font="20px arial";
-        ctx.fillStyle="black";
-        ctx.fillText('ε',((x2-50)+x1)/2,this.y-60);
-        ctx.stroke();
-        ctx.lineTo((x1-25)+5,(y1-25)-20);
-        ctx.lineTo((x1-25),(y1-25));
-        ctx.lineTo((x1-25)+20,(y1-25)-5);
+        this.ctx.moveTo(x2-25,y2-25);
+        this.ctx.quadraticCurveTo(((x2-50)+x1)/2,this.y-75,x1-25,y1-25);
+        this.ctx.font="20px arial";
+        this.ctx.fillStyle="black";
+        this.ctx.fillText('ε',((x2-50)+x1)/2,this.y-60);
+        this.ctx.stroke();
+        this.ctx.lineTo((x1-25)+5,(y1-25)-20);
+        this.ctx.lineTo((x1-25),(y1-25));
+        this.ctx.lineTo((x1-25)+20,(y1-25)-5);
     }
     linea_eps(x1,x2,y1,y2){
-        var canvas = document.getElementById("lienzo");
-        var ctx = canvas.getContext("2d");
-        ctx.moveTo(x1-25,y1+25);
-        ctx.quadraticCurveTo(((x2-50)+x1)/2,this.y+100,x2-25,y2+25);
-        ctx.font="20px arial";
-        ctx.fillStyle="black";
-        ctx.fillText('ε',((x2-50)+x1)/2,this.y+50);
-        ctx.lineTo((x2-25)-5,(y2+25)+20);
-        ctx.lineTo((x2-25),(y2+25));
-        ctx.lineTo((x2-25)-20,(y2+25)+5);
-        ctx.stroke();
+        this.ctx.moveTo(x1-25,y1+25);
+        this.ctx.quadraticCurveTo(((x2-50)+x1)/2,this.y+100,x2-25,y2+25);
+        this.ctx.font="20px arial";
+        this.ctx.fillStyle="black";
+        this.ctx.fillText('ε',((x2-50)+x1)/2,this.y+50);
+        this.ctx.lineTo((x2-25)-5,(y2+25)+20);
+        this. ctx.lineTo((x2-25),(y2+25));
+        this.ctx.lineTo((x2-25)-20,(y2+25)+5);
+        this.ctx.stroke();
+    }
+    flecha_or(simb){
+        this.ctx.moveTo(this.xi,this.yi);
+        this.ctx.quadraticCurveTo(((this.xf-50)+this.xi)/2,this.y+100,this.xf-25,this.yf+25);
+        this.ctx.font="20px arial";
+        this.ctx.fillStyle="black";
+        this.ctx.fillText(simb,((this.x2)+this.x1)/2,this.y+100);
+        this.ctx.lineTo((this.xf-25)-5,(this.yf+25)+20);
+        this.ctx.lineTo((this.xf-25),(this.yf+25));
+        this.ctx.lineTo((this.xf-25)-20,(this.yf+25)+5);
+        this.ctx.stroke();
     }
     estado(x1,y1){
         var txt = document.querySelector("canvas").getContext("2d");
@@ -114,22 +121,37 @@ class Dibujo{
         this.flecha(simb);
         this.circulo();
     }
+    concatenacion_or(simb){
+        this.flecha_or(simb);
+        this.circulo();
+    }
     orr(){
-
+        this.x=this.xi;
+        this.y=this.yi;
+        //this.i-=this.top;
+        this.y+=100;
+        this.flecha_or('z');
     }
     estado_inicial(){
         this.linea();
         this.circulo();
+        this.xi=this.x-25;
+        this.yi=this.y+25;
+        this.ctx.save();
     }
     estado_final(){
-        var canvas = document.getElementById("lienzo");
-        var ctx = canvas.getContext("2d");
-        ctx.lineWidth = 5;
-        ctx.moveTo(this.x,this.y);
-        ctx.arc(this.x-25,this.y,25,0,2*Math.PI,true);
-        ctx.moveTo(this.x-7,this.y);
-        ctx.arc(this.x-25,this.y,18,0,2*Math.PI,true);
-        ctx.stroke();
+        this.ctx.restore();
+        this.ctx.save();
+        //this.ctx.strokeStyle='red';
+        //this.ctx.fillStyle='red';
+        this.ctx.lineWidth = 5;
+        this.ctx.moveTo(this.x,this.y);
+        this.ctx.arc(this.x-25,this.y,25,0,2*Math.PI,true);
+        this. ctx.moveTo(this.x-7,this.y);
+        this.ctx.arc(this.x-25,this.y,18,0,2*Math.PI,true);
+        this.ctx.stroke();
+        this.ctx.restore();
+
     }
     recorrido(){
         this.estado_inicial();
@@ -137,24 +159,57 @@ class Dibujo{
             case 0:
                 break;
             case 1:
-                this.concatenacion(this.exp);
-                break;
-            default:
-                for(this.j=0;this.j<this.exp.length;this.j++){
-                    this.analiza(this.exp[this.j]);
+                this.validar(this.exp[0]);
+                if(this.valid){
+                    this.concatenacion(this.exp);
+                    break;
                 }
-                do{
-                    this.pop();
-                }while(this.front<this.top);
-        }
-        this.estado_final();
-        alert("fin");
+                else{
+                    break;
+                }
+            default:
+                this.validar(this.exp[0]);
+                if(this.valid){
+                    for(this.j=0;this.j<this.exp.length;this.j++){
+                        this.analiza(this.exp[this.j]);
+                    }
+                    do{
+                        this.pop();
+                    }while(this.front<this.top);
+                    break;
+                }
+                else{
+                    break;
+                }
 
+        }
+        if(this.valid){
+            this.estado_final();
+        }
+        else{
+            this.borrar();
+        }
+    }
+    validar(dato){
+        if(dato=='*'||dato=='|'){
+            switch(dato){
+                case '*':
+                    alert('El símbolo " * " está reservado.');
+                    break;
+                case '|':
+                    alert('El símbolo " | " está reservado.');
+                    break;
+            }
+            this.valid=false;
+        }
     }
     analiza(dato){
         if(dato=='*'||dato=='|'||dato=='('||dato==')'){
             switch(dato){
                 case ')':
+                    if(this.exp[this.j+1]!='*'){
+
+                    }
                     break;
                 case '(':
                     do{
@@ -169,15 +224,28 @@ class Dibujo{
                         this.limpiar_cola();
                     }
                     else{
-                        do{
-                            this.pop();
-                        }while(this.front<this.top-1);
-                        this.limpiar_cola();
-                        this.cerradura(this.exp[this.j-1]);
+                        if(this.front==this.top-1){
+                            this.cerradura(this.exp[this.j-1]);
+                            this.limpiar_cola();
+                        }
+                        else{
+                            do{
+                                this.pop();
+                            }while(this.front<this.top-1);
+                            this.limpiar_cola();
+                            this.cerradura(this.exp[this.j-1]);
+                        }
                     }
                     break;
                     
                 case '|':
+                    do{
+                        this.pop();
+                    }while(this.front<this.top);
+                    this.limpiar_cola();
+                    this.xf=this.x;
+                    this.yf=this.y;
+                    this.orr();
                     break;
             }
         }
@@ -189,9 +257,6 @@ class Dibujo{
        if(this.top<=50){
            this.cola[this.top]=dato;
            this.top++;
-       } 
-       else{
-           alert("La cola está llena");
        }
     }
     pop(){
@@ -205,9 +270,6 @@ class Dibujo{
             }
             
         }
-        else{
-            alert("La cola está vacía");
-        }
     }
     limpiar_cola(){
         this.cola=0;
@@ -215,32 +277,18 @@ class Dibujo{
         this.front=0;
         this.top=0;
     }
-}
-
-class Goma{
     borrar(){
         var canvas = document.getElementById("lienzo");
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle="#6DD5FA";
-        ctx.fillRect(0,0,3000,500);
-        ctx.fill();
+        canvas.width = canvas.width;
     }
-}
-class Recorre{
-    constructor(exp,op_top,top,pila){
-        this.exp;
-
-    }
-
 }
 
 document.getElementById('Formulario')
     .addEventListener('submit', function(e){
        // alert("1");
         const exp = document.getElementById('exp_reg').value;
-        const graf = new Dibujo(exp);
-        const borra = new Goma();
-        borra.borrar();
+        const graf = new Dibujo(exp.toLowerCase());
+        graf.borrar();
         //graf.estado_inicial();
         //graf.cerradura();
         //graf.concatenacion();
